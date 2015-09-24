bb5542b9cea0b93863a20f0d04f4f528c73d07ad
Tom Haskins-Vaughan <tomhv@janeiredale.com>
2015-09-24 09:05:04 -0400

    First draft of issue

--- src/Model/Issue.php
+++ src/Model/Issue.php
@@ -19,4 +19,107 @@ namespace ChurchStreet\LabCoat\Model;
  */
 class Issue
 {
+    /**
+     * apiData
+     *
+     * @var array
+     */
+    private $apiData;
+
+    /**
+     * metaData
+     *
+     * @var array
+     */
+    private $metaData;
+
+    /**
+     * __construct()
+     *
+     * @author Tom Haskins-Vaughan <tom@tomhv.uk>
+     * @since  0.1.0
+     *
+     * @param array $apiData
+     */
+    public function __construct(array $apiData)
+    {
+        $this->apiData = $apiData;
+        $this->metaData = $this->getMetaData($apiData);
+    }
+
+    /**
+     * __get()
+     *
+     * @author Tom Haskins-Vaughan <tom@tomhv.uk>
+     * @since  0.1.0
+     *
+     * @return mixed
+     */
+    public function __get($key)
+    {
+        $key = lcfirst($key);
+
+        if (array_key_exists($key, $this->metaData)) {
+            return $this->metaData[$key];
+        }
+
+        if (array_key_exists($key, $this->apiData)) {
+            return $this->apiData[$key];
+        }
+
+        throw new \InvalidArgumentException(sprintf(
+            'Cannot access %s',
+            $key
+        ));
+    }
+
+    /**
+     * __isset()
+     *
+     * @author Tom Haskins-Vaughan <tom@tomhv.uk>
+     * @since  0.1.0
+     *
+     * @return mixed
+     */
+    public function __isset($key)
+    {
+        $key = lcfirst($key);
+
+        if (array_key_exists($key, $this->metaData)) {
+            return true;
+        }
+
+        if (array_key_exists($key, $this->apiData)) {
+            return true;
+        }
+
+        false;
+    }
+
+    /**
+     * Parse apiData for metaData
+     *
+     * @author Tom Haskins-Vaughan <tom@tomhv.uk>
+     * @since  0.1.0
+     *
+     * @param array
+     */
+    private function getMetaData($apiData)
+    {
+        $metaData = [
+            'estimated' => 0,
+            'actual' => 0,
+        ];
+
+        $pattern = '/<!--\DO NOT EDIT OR REMOVE THIS LINE({.*})-->/';
+
+        if (preg_match($pattern, $this->apiData['description'], $matches)) {
+            $meta = json_decode($matches[1], true);
+
+            $metaData['estimated'] = $meta['estimated'];
+            $metaData['actual'] = $meta['actual'];
+        }
+
+        return $metaData;
+    }
 }
